345526c75e7f75c186720756ba7ddf9fc5daa3f2
Richard Harrah <topplethenunnery@gmail.com>
2024-06-21 20:24:47 -0400

    add AP ratios and some multipliers

--- app/data/seasons.ts
+++ app/data/seasons.ts
@@ -2,13 +2,45 @@ const UNKNOWN_SEASON_START_OR_ENDING = null;
 
 export type AlternativeTooltipSource = "ptr" | "beta";
 
+export type BombOrCleave = "bomb" | "cleave" | "both";
+
+export interface AbilityMultiplier {
+  ability: BombOrCleave;
+  appliedName: string;
+  value: number;
+}
+
+export interface Conditional {
+  notAppliedName: string;
+}
+
+export interface Stacking {
+  maxStacks: number;
+}
+
+export interface ConditionalAbilityMultiplier
+  extends AbilityMultiplier,
+    Conditional {}
+
+export interface StackingAbilityMultiplier
+  extends AbilityMultiplier,
+    Stacking {}
+
 export interface Season {
   name: string;
   shortName: string;
+  description: string;
   slug: string;
   startDate: number | null;
   endDate: number | null;
   seasonIcon: string;
+  apRatios: {
+    soulCleave: number;
+    spiritBombPerSoulFragment: number;
+  };
+  alwaysOnMultipliers: AbilityMultiplier[];
+  conditionalMultipliers: ConditionalAbilityMultiplier[];
+  stackingMultipliers: StackingAbilityMultiplier[];
   tooltipSource?: AlternativeTooltipSource;
 }
 
@@ -16,35 +48,232 @@ export const seasons: readonly Season[] = [
   {
     name: "TWW S1",
     shortName: "TWW1",
+    description: "The War Within season 1",
     slug: "tww-season-1",
     startDate: UNKNOWN_SEASON_START_OR_ENDING,
     endDate: UNKNOWN_SEASON_START_OR_ENDING,
     seasonIcon: "/img/thewarwithin/season1/logo.png",
+    apRatios: {
+      soulCleave: 1.4812,
+      spiritBombPerSoulFragment: 0.35041,
+    },
+    alwaysOnMultipliers: [
+      {
+        ability: "bomb",
+        appliedName: "Chaos Brand",
+        value: 1.05,
+      },
+      {
+        ability: "cleave",
+        appliedName: "Armor Mitigation",
+        value: 0.7,
+      },
+    ],
+    conditionalMultipliers: [
+      {
+        ability: "cleave",
+        appliedName: "Mystic Touch",
+        notAppliedName: "No Mystic Touch",
+        value: 1.05,
+      },
+      {
+        ability: "cleave",
+        appliedName: "Focused Cleave",
+        notAppliedName: "No Focused Cleave",
+        value: 1.5,
+      },
+      {
+        ability: "bomb",
+        appliedName: "Burning Blood",
+        notAppliedName: "No Burning Blood",
+        value: 1.1,
+      },
+      {
+        ability: "bomb",
+        appliedName: "Fiery Demise",
+        notAppliedName: "No Fiery Demise",
+        value: 1.4,
+      },
+    ],
+    stackingMultipliers: [],
     tooltipSource: "beta",
   },
   {
     name: "DF S4",
     shortName: "DF4",
+    description: "Dragonflight season 4",
     slug: "df-season-4",
     startDate: 1_713_884_400_000,
     endDate: UNKNOWN_SEASON_START_OR_ENDING,
     seasonIcon: "/img/dragonflight/season4/logo.png",
+    apRatios: {
+      soulCleave: 1.4812,
+      spiritBombPerSoulFragment: 0.35041,
+    },
+    alwaysOnMultipliers: [
+      {
+        ability: "bomb",
+        appliedName: "Chaos Brand",
+        value: 1.05,
+      },
+      {
+        ability: "cleave",
+        appliedName: "Armor Mitigation",
+        value: 0.7,
+      },
+    ],
+    conditionalMultipliers: [
+      {
+        ability: "cleave",
+        appliedName: "Mystic Touch",
+        notAppliedName: "No Mystic Touch",
+        value: 1.05,
+      },
+      {
+        ability: "cleave",
+        appliedName: "Focused Cleave",
+        notAppliedName: "No Focused Cleave",
+        value: 1.5,
+      },
+      {
+        ability: "bomb",
+        appliedName: "Burning Blood",
+        notAppliedName: "No Burning Blood",
+        value: 1.1,
+      },
+      {
+        ability: "bomb",
+        appliedName: "Fiery Demise",
+        notAppliedName: "No Fiery Demise",
+        value: 1.5,
+      },
+    ],
+    stackingMultipliers: [
+      {
+        ability: "both",
+        appliedName: "Fiery Resolve",
+        value: 1.02,
+        maxStacks: 5,
+      },
+    ],
   },
   {
     name: "DF S3",
     shortName: "DF3",
+    description: "Dragonflight season 3",
     slug: "df-season-3",
     startDate: 1_699_974_000_000,
     endDate: 1_713_848_400_000,
     seasonIcon: "/img/dragonflight/season3/logo.png",
+    apRatios: {
+      soulCleave: 1.61,
+      spiritBombPerSoulFragment: 0.35041,
+    },
+    alwaysOnMultipliers: [
+      {
+        ability: "bomb",
+        appliedName: "Chaos Brand",
+        value: 1.05,
+      },
+      {
+        ability: "cleave",
+        appliedName: "Armor Mitigation",
+        value: 0.7,
+      },
+    ],
+    conditionalMultipliers: [
+      {
+        ability: "cleave",
+        appliedName: "Mystic Touch",
+        notAppliedName: "No Mystic Touch",
+        value: 1.05,
+      },
+      {
+        ability: "cleave",
+        appliedName: "Focused Cleave",
+        notAppliedName: "No Focused Cleave",
+        value: 1.5,
+      },
+      {
+        ability: "bomb",
+        appliedName: "Burning Blood",
+        notAppliedName: "No Burning Blood",
+        value: 1.1,
+      },
+      {
+        ability: "bomb",
+        appliedName: "Fiery Demise",
+        notAppliedName: "No Fiery Demise",
+        value: 1.5,
+      },
+    ],
+    stackingMultipliers: [
+      {
+        ability: "both",
+        appliedName: "Fiery Resolve",
+        value: 1.02,
+        maxStacks: 5,
+      },
+    ],
   },
   {
     name: "DF S2",
     shortName: "DF2",
+    description: "Dragonflight season 2",
     slug: "df-season-2",
     startDate: 1_683_644_400_000,
     endDate: 1_699_336_800_000,
     seasonIcon: "/img/dragonflight/season2/logo.png",
+    apRatios: {
+      soulCleave: 1.31,
+      spiritBombPerSoulFragment: 0.32445,
+    },
+    alwaysOnMultipliers: [
+      {
+        ability: "bomb",
+        appliedName: "Chaos Brand",
+        value: 1.05,
+      },
+      {
+        ability: "cleave",
+        appliedName: "Armor Mitigation",
+        value: 0.7,
+      },
+    ],
+    conditionalMultipliers: [
+      {
+        ability: "cleave",
+        appliedName: "Mystic Touch",
+        notAppliedName: "No Mystic Touch",
+        value: 1.05,
+      },
+      {
+        ability: "cleave",
+        appliedName: "Focused Cleave",
+        notAppliedName: "No Focused Cleave",
+        value: 1.4,
+      },
+      {
+        ability: "bomb",
+        appliedName: "Burning Blood",
+        notAppliedName: "No Burning Blood",
+        value: 1.1,
+      },
+      {
+        ability: "bomb",
+        appliedName: "Fiery Demise",
+        notAppliedName: "No Fiery Demise",
+        value: 1.4,
+      },
+    ],
+    stackingMultipliers: [
+      {
+        ability: "bomb",
+        appliedName: "Fires of Fel",
+        value: 1.02,
+        maxStacks: 6,
+      },
+    ],
   },
 ];
 
--- app/routes/season+/$season.tsx
+++ app/routes/season+/$season.tsx
@@ -3,11 +3,36 @@ import { useLoaderData } from "@remix-run/react";
 import type { LoaderFunctionArgs } from "@vercel/remix";
 import { json } from "@vercel/remix";
 
-import { H1, H2, Lead } from "~/components/typography.tsx";
+import { H1, H2, H3, H4, Lead, Paragraph } from "~/components/typography.tsx";
+import type {
+  AbilityMultiplier,
+  ConditionalAbilityMultiplier,
+  StackingAbilityMultiplier,
+} from "~/data/seasons.ts";
 import { findSeasonByName } from "~/data/seasons.ts";
 import { serverTiming } from "~/lib/constants.ts";
 import { combineHeaders } from "~/lib/misc.ts";
 import { makeTimings } from "~/lib/timing.server.ts";
+
+interface MultiplierResult {
+  value: number;
+  appliedMultipliers: string[];
+}
+
+function abilityMultiplierAppliedName(multiplier: AbilityMultiplier) {
+  return `${multiplier.appliedName} (${String(multiplier.value)})`;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function abilityMultiplierNotAppliedName(
+  multiplier: ConditionalAbilityMultiplier,
+) {
+  return multiplier.notAppliedName;
+}
+
+function stackingMultiplierAppliedName(multiplier: StackingAbilityMultiplier) {
+  return `${multiplier.appliedName} (${String(multiplier.value)} per stack [max ${String(multiplier.maxStacks)}])`;
+}
 
 export function loader({ params }: LoaderFunctionArgs) {
   invariantResponse(
@@ -20,8 +45,41 @@ export function loader({ params }: LoaderFunctionArgs) {
 
   const timings = makeTimings(`${season.name} loader`);
 
+  const soulCleaveBaseApRatio = season.alwaysOnMultipliers
+    .filter((it) => it.ability === "cleave" || it.ability === "both")
+    .reduce<MultiplierResult>(
+      (acc, multiplier) => ({
+        value: acc.value * multiplier.value,
+        appliedMultipliers: [
+          ...acc.appliedMultipliers,
+          abilityMultiplierAppliedName(multiplier),
+        ],
+      }),
+      {
+        value: season.apRatios.soulCleave,
+        appliedMultipliers: [],
+      },
+    );
+  const spiritBombBaseApRatioPerSoulFragment = season.alwaysOnMultipliers
+    .filter((it) => it.ability === "bomb" || it.ability === "both")
+    .reduce<MultiplierResult>(
+      (acc, multiplier) => ({
+        value: acc.value * multiplier.value,
+        appliedMultipliers: [
+          ...acc.appliedMultipliers,
+          abilityMultiplierAppliedName(multiplier),
+        ],
+      }),
+      {
+        value: season.apRatios.spiritBombPerSoulFragment,
+        appliedMultipliers: [],
+      },
+    );
+
   const enhancedSeason = {
     ...season,
+    soulCleaveBaseApRatio,
+    spiritBombBaseApRatioPerSoulFragment,
   };
 
   return json(
@@ -61,17 +119,109 @@ export default function SeasonRoute() {
   return (
     <>
       <div className="pb-8 space-y-2">
-        <H1>Should I Spirit Bomb or Should I Soul Cleave?</H1>
+        <H1>Should I Spirit Bomb or should I Soul Cleave?</H1>
         <Lead>
-          Efficiency calculator for Spirit Bomb and Soul Cleave in {season.name}
-          .
+          Efficiency calculator for Spirit Bomb and Soul Cleave in{" "}
+          {season.description}.
         </Lead>
       </div>
-      <section className="hidden md:block">
-        <div className="overflow-hidden rounded-lg border bg-background px-4 shadow">
-          <div className="flex h-[50vh] flex-col items-center justify-center gap-2">
-            <H2>We&apos;re building here</H2>
-            <Lead>Hopefully it&apos;ll be neat.</Lead>
+      <section className="pb-8 space-y-3">
+        <div className="space-y-2">
+          <H2>Soul Cleave</H2>
+          <Lead>Base AP Ratio: {season.apRatios.soulCleave}</Lead>
+          <Lead>Effective AP Ratio: {season.soulCleaveBaseApRatio.value}</Lead>
+        </div>
+        <div className="space-y-2">
+          <H3>Applied Multipliers</H3>
+          <Paragraph>
+            {season.soulCleaveBaseApRatio.appliedMultipliers.join(", ")}
+          </Paragraph>
+        </div>
+        <div className="space-y-2">
+          <H3>Available Multipliers</H3>
+          <div className="space-y-2">
+            <H4>Always On Multipliers</H4>
+            <Paragraph>
+              {season.alwaysOnMultipliers
+                .filter(
+                  (it) => it.ability === "cleave" || it.ability === "both",
+                )
+                .map(abilityMultiplierAppliedName)
+                .join(", ")}
+            </Paragraph>
+          </div>
+          <div className="space-y-2">
+            <H4>Conditional Multipliers</H4>
+            <Paragraph>
+              {season.conditionalMultipliers
+                .filter(
+                  (it) => it.ability === "cleave" || it.ability === "both",
+                )
+                .map(abilityMultiplierAppliedName)
+                .join(", ")}
+            </Paragraph>
+          </div>
+          <div className="space-y-2">
+            <H4>Stacking Multipliers</H4>
+            <Paragraph>
+              {season.stackingMultipliers
+                .filter(
+                  (it) => it.ability === "cleave" || it.ability === "both",
+                )
+                .map(stackingMultiplierAppliedName)
+                .join(", ")}
+            </Paragraph>
+          </div>
+        </div>
+      </section>
+      <section className="pb-8 space-y-3">
+        <div className="space-y-2">
+          <H2>Spirit Bomb</H2>
+          <Lead>
+            Base AP Ratio per Soul Fragment:{" "}
+            {season.apRatios.spiritBombPerSoulFragment}
+          </Lead>
+          <Lead>
+            Effective AP Ratio per Soul Fragment:{" "}
+            {season.spiritBombBaseApRatioPerSoulFragment.value}
+          </Lead>
+        </div>
+        <div className="space-y-2">
+          <H3>Applied Multipliers</H3>
+          <Paragraph>
+            {season.spiritBombBaseApRatioPerSoulFragment.appliedMultipliers.join(
+              ", ",
+            )}
+          </Paragraph>
+        </div>
+        <div className="space-y-2">
+          <H3>Available Multipliers</H3>
+          <div className="space-y-2">
+            <H4>Always On Multipliers</H4>
+            <Paragraph>
+              {season.alwaysOnMultipliers
+                .filter((it) => it.ability === "bomb" || it.ability === "both")
+                .map(abilityMultiplierAppliedName)
+                .join(", ")}
+            </Paragraph>
+          </div>
+          <div className="space-y-2">
+            <H4>Conditional Multipliers</H4>
+            <Paragraph>
+              {season.conditionalMultipliers
+                .filter((it) => it.ability === "bomb" || it.ability === "both")
+                .map(abilityMultiplierAppliedName)
+                .join(", ")}
+            </Paragraph>
+          </div>
+          <div className="space-y-2">
+            <H4>Stacking Multipliers</H4>
+            <Paragraph>
+              {season.stackingMultipliers
+                .filter((it) => it.ability === "bomb" || it.ability === "both")
+                .map(stackingMultiplierAppliedName)
+                .join(", ")}
+            </Paragraph>
           </div>
         </div>
       </section>
